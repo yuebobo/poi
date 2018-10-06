30f72f06d22a4060f58f59e59fe02bb4dafafa27
zhouYueBo <1304139354@qq.com>
2018-10-06 17:41:00 +0800

    the first local

---
+++ src/main/java/com/entity/Constants.java
@@ -0,0 +1,20 @@
+package com.entity;
+
+public  class Constants {
+
+	public static final String T1X = "T1X";
+	public static final String T2X = "T2X";
+	public static final String T3X = "T3X";
+	public static final String T4X = "T4X";
+	public static final String T5X = "T5X";
+	public static final String R1X = "R1X";
+	public static final String R2X = "R2X";
+	
+	public static final String T1Y = "T1Y";
+	public static final String T2Y = "T2Y";
+	public static final String T3Y = "T3Y";
+	public static final String T4Y = "T4Y";
+	public static final String T5Y = "T5Y";
+	public static final String R1Y = "R1Y";
+	public static final String R2Y = "R2Y";
+}
---
+++ src/main/java/com/test/GetExcelTest.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import com.file.GetExcelValue;
+
+public class GetExcelTest {
+
+
+    public static void  main(String args[]){
+
+        int a = 4%2;
+        System.out.println(a);
+
+//        String path = "C:\\Users\\lizhongxiang\\Desktop\\数据\\工作簿4.xlsx";
+//        GetExcelValue.getEarthquakeDamperDisEnergyY(path);
+
+    }
+}
