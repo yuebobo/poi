942574062e76fef54a4bb31c114490b3e872ff70
zhouYueBo <1304139354@qq.com>
2018-10-08 22:59:27 +0800

    最后三个表的结果基本正确，结论还没比较加上

--- src/main/java/com/insert/CaculateTable.java
+++ src/main/java/com/insert/CaculateTable.java
@@ -64,40 +64,40 @@ public class CaculateTable {
         //3.根据公式进行计算
         //1).受弯验算
         Double h0 = Double.valueOf(sectionH) - Double.valueOf(_afS);
-        Double _afs_ = Double.valueOf(stressConditionM) / (Double.valueOf(_af1) * Double.valueOf(concreteGradeFck) * Double.valueOf(sectionB) * h0 * h0);
-        Double _ypclong_ = 1.00 - Math.pow(Double.valueOf(1 - 2 * _afs_),0.5);
+        Double _afs_ = Double.valueOf(stressConditionM) * 1000000 / (Double.valueOf(_af1) * Double.valueOf(concreteGradeFck) * Double.valueOf(sectionB) * h0 * h0);
+        Double _ypclong_ = 1.00 - Math.pow(Double.valueOf(1 - (2 * _afs_)),0.5);
         Double _As_ = _ypclong_ * Double.valueOf(sectionB) * h0 * Double.valueOf(_af1) * Double.valueOf(concreteGradeFck) / Double.valueOf(steelGradeFstk);
         Double _Ps_ = _As_ / (Double.valueOf(sectionB) * h0);
 
         //1).受剪验算
-        Double _shearForce1_ = 0.2 * Double.valueOf(concreteGradeFck) * Double.valueOf(sectionB) * h0 / _yre;
+        Double _shearForce1_ = 0.2 * Double.valueOf(concreteGradeFck) * Double.valueOf(sectionB) * h0 / (_yre * 1000);
         Double _shearForce2_ = ((0.6 * _afCV * Double.valueOf(concreteGradeFtk) * Double.valueOf(sectionB) * h0)+
-                ((Double.valueOf(steelGradeFyvk) * Double.valueOf(sectionH) * 0.25 * Math.PI * Math.pow(Double.valueOf(hoopD),2) * h0) / 8)) /_yre;
+                ((Double.valueOf(steelGradeFyvk) * 4 * 0.25 * Math.PI * Math.pow(Double.valueOf(hoopD),2) * h0) / Double.valueOf(hoopL))) / (_yre * 1000);
         System.out.println("=============== 计算结果 ===================");
         System.out.println("受弯验算 ：\n_afs_  = "+ _afs_ + "\n_ypclong_ = " + _ypclong_ + "\n_As_ = "+ _As_ + "\n_Ps_ = "+_Ps_ );
         System.out.println("受剪验算 ：\n_shearForce1_  = "+ _shearForce1_ + "\n_shearForce1_ = " + _shearForce1_ );
 
         //4.将计算的结果回填到表格中
-        Util.insertValueToCell(table.getRow(0).getCell(2),sectionB);
-        Util.insertValueToCell(table.getRow(0).getCell(5),sectionH);
-        Util.insertValueToCell(table.getRow(1).getCell(2),stressConditionV);
-        Util.insertValueToCell(table.getRow(1).getCell(5),stressConditionM);
+        Util.insertValueToCell(table.getRow(0).getCell(2),Util.getPrecisionString(sectionB,1));
+        Util.insertValueToCell(table.getRow(0).getCell(5),Util.getPrecisionString(sectionH,1));
+        Util.insertValueToCell(table.getRow(1).getCell(2),Util.getPrecisionString(stressConditionV,1));
+        Util.insertValueToCell(table.getRow(1).getCell(5),Util.getPrecisionString(stressConditionM,1));
         Util.insertValueToCell(table.getRow(2).getCell(2),concreteGrade);
-        Util.insertValueToCell(table.getRow(3).getCell(2),concreteGradeFck);
-        Util.insertValueToCell(table.getRow(3).getCell(5),_afS);
-        Util.insertValueToCell(table.getRow(4).getCell(2),concreteGradeFc);
-        Util.insertValueToCell(table.getRow(4).getCell(5),concreteGradeFtk);
+        Util.insertValueToCell(table.getRow(3).getCell(2),Util.getPrecisionString(concreteGradeFck,2));
+        Util.insertValueToCell(table.getRow(3).getCell(5),Util.getPrecisionString(_afS,2));
+        Util.insertValueToCell(table.getRow(4).getCell(2),Util.getPrecisionString(concreteGradeFc,2));
+        Util.insertValueToCell(table.getRow(4).getCell(5),Util.getPrecisionString(concreteGradeFtk,2));
         Util.insertValueToCell(table.getRow(5).getCell(2),steelGrade);
-        Util.insertValueToCell(table.getRow(6).getCell(2),steelGradeFstk);
-        Util.insertValueToCell(table.getRow(6).getCell(5),steelGradeFyvk);
-        Util.insertValueToCell(table.getRow(7).getCell(2),hoopD);
-        Util.insertValueToCell(table.getRow(7).getCell(5),hoopL);
-        Util.insertValueToCell(table.getRow(8).getCell(2),_afs_.toString());
-        Util.insertValueToCell(table.getRow(8).getCell(4),_ypclong_.toString());
-        Util.insertValueToCell(table.getRow(9).getCell(2),_As_.toString());
-        Util.insertValueToCell(table.getRow(9).getCell(5),_Ps_.toString());
-        Util.insertValueToCell(table.getRow(10).getCell(2),_shearForce1_.toString());
-        Util.insertValueToCell(table.getRow(11).getCell(2),_shearForce1_.toString());
+        Util.insertValueToCell(table.getRow(6).getCell(2),Util.getPrecisionString(steelGradeFstk,0));
+        Util.insertValueToCell(table.getRow(6).getCell(5),Util.getPrecisionString(steelGradeFyvk,0));
+        Util.insertValueToCell(table.getRow(7).getCell(2),Util.getPrecisionString(hoopD,0));
+        Util.insertValueToCell(table.getRow(7).getCell(5),Util.getPrecisionString(hoopL,0));
+        Util.insertValueToCell(table.getRow(8).getCell(2),Util.getPrecisionString(_afs_,3));
+        Util.insertValueToCell(table.getRow(8).getCell(4),Util.getPrecisionString(_ypclong_,3));
+        Util.insertValueToCell(table.getRow(9).getCell(2),Util.getPrecisionString(_As_,0));
+        Util.insertValueToCell(table.getRow(9).getCell(5),Util.getPrecisionString(_Ps_ * 100,2)+"%");
+        Util.insertValueToCell(table.getRow(10).getCell(2),Util.getPrecisionString(_shearForce1_,0));
+        Util.insertValueToCell(table.getRow(11).getCell(2),Util.getPrecisionString(_shearForce2_,0));
     }
 
     /**
@@ -153,35 +153,35 @@ public class CaculateTable {
 
         //3.根据公式进行计算
         Double h0 = Double.valueOf(sectionH) - Double.valueOf(_afS);
-        Double _result1_ = 0.2 * Double.valueOf(concreteGradeFck) * Double.valueOf(sectionB) * h0 / _yre;
+        Double _result1_ = 0.2 * Double.valueOf(concreteGradeFck) * Double.valueOf(sectionB) * h0 / (_yre * 1000 );
         Double _lamda_ = (Double.valueOf(floorH) - Double.valueOf(_afS)) / (2 * h0);
         Double _result3_ = (((1.05 * Double.valueOf(concreteGradeFtk) * Double.valueOf(sectionB) * h0) / (_lamda_ + 1.00)) +
-                            (Double.valueOf(steelGradeFyvk) * 0.25 * Math.PI * Math.pow(Double.valueOf(hoopD),2) * h0 / Double.valueOf(hoopL)) +
-                            (0.056 * Double.valueOf(stressConditionP) * 1000))/_yre;
+                            (Double.valueOf(steelGradeFyvk) * 4 * 0.25 * Math.PI * Math.pow(Double.valueOf(hoopD),2) * h0 / Double.valueOf(hoopL)) +
+                            (0.056 * Double.valueOf(stressConditionP) * 1000))/ (_yre * 1000);
         System.out.println("=============== 计算结果 ===================");
         System.out.println("受剪验算 ：\n_result1_  = "+ _result1_ + "\n_lamda_ = " + _lamda_ + "\n_result3_ =  "+_result3_ );
 
         //4.将计算的结果回填到表格中
-        Util.insertValueToCell(table.getRow(0).getCell(2),sectionB);
-        Util.insertValueToCell(table.getRow(0).getCell(5),sectionH);
-        Util.insertValueToCell(table.getRow(1).getCell(1),floorH);
-        Util.insertValueToCell(table.getRow(2).getCell(2),stressConditionV);
-        Util.insertValueToCell(table.getRow(2).getCell(5),stressConditionM);
-        Util.insertValueToCell(table.getRow(3).getCell(2),stressConditionP);
+        Util.insertValueToCell(table.getRow(0).getCell(2),Util.getPrecisionString(sectionB,1));
+        Util.insertValueToCell(table.getRow(0).getCell(5),Util.getPrecisionString(sectionH,1));
+        Util.insertValueToCell(table.getRow(1).getCell(1),Util.getPrecisionString(floorH,0));
+        Util.insertValueToCell(table.getRow(2).getCell(2),Util.getPrecisionString(stressConditionV,1));
+        Util.insertValueToCell(table.getRow(2).getCell(5),Util.getPrecisionString(stressConditionM,1));
+        Util.insertValueToCell(table.getRow(3).getCell(2),Util.getPrecisionString(stressConditionP,1));
         Util.insertValueToCell(table.getRow(4).getCell(2),concreteGrade);
-        Util.insertValueToCell(table.getRow(5).getCell(2),concreteGradeFck);
-        Util.insertValueToCell(table.getRow(5).getCell(5),_afS);
-        Util.insertValueToCell(table.getRow(6).getCell(2),concreteGradeFc);
-        Util.insertValueToCell(table.getRow(6).getCell(5),concreteGradeFtk);
+        Util.insertValueToCell(table.getRow(5).getCell(2),Util.getPrecisionString(concreteGradeFck,2));
+        Util.insertValueToCell(table.getRow(5).getCell(5),Util.getPrecisionString(_afS,2));
+        Util.insertValueToCell(table.getRow(6).getCell(2),Util.getPrecisionString(concreteGradeFc,2));
+        Util.insertValueToCell(table.getRow(6).getCell(5),Util.getPrecisionString(concreteGradeFtk,2));
         Util.insertValueToCell(table.getRow(7).getCell(2),steelGrade);
-        Util.insertValueToCell(table.getRow(8).getCell(2),steelGradeFstk);
-        Util.insertValueToCell(table.getRow(8).getCell(5),steelGradeFyvk);
-        Util.insertValueToCell(table.getRow(9).getCell(2),hoopD);
-        Util.insertValueToCell(table.getRow(9).getCell(5),hoopL);
+        Util.insertValueToCell(table.getRow(8).getCell(2),Util.getPrecisionString(steelGradeFstk,0));
+        Util.insertValueToCell(table.getRow(8).getCell(5),Util.getPrecisionString(steelGradeFyvk,0));
+        Util.insertValueToCell(table.getRow(9).getCell(2),Util.getPrecisionString(hoopD,0));
+        Util.insertValueToCell(table.getRow(9).getCell(5),Util.getPrecisionString(hoopL,0));
 
-        Util.insertValueToCell(table.getRow(10).getCell(2),_result1_.toString());
-        Util.insertValueToCell(table.getRow(11).getCell(2),_lamda_.toString());
-        Util.insertValueToCell(table.getRow(12).getCell(2),_result3_.toString());
+        Util.insertValueToCell(table.getRow(10).getCell(2),Util.getPrecisionString(_result1_,0));
+        Util.insertValueToCell(table.getRow(11).getCell(2),Util.getPrecisionString(_lamda_,2));
+        Util.insertValueToCell(table.getRow(12).getCell(2),Util.getPrecisionString(_result3_,0));
     }
 
     /**
@@ -244,40 +244,40 @@ public class CaculateTable {
         //3.根据公式进行计算
         Double h0 = Double.valueOf(sectionH) - Double.valueOf(_afS);
         Double _M_ = (Double.valueOf(floorH) - Double.valueOf(damperH)) * Double.valueOf(stressConditionV) * 0.5;
-        Double _As_ = _M_ / (Double.valueOf(steelGradeFstk) * (Double.valueOf(sectionH ) - Double.valueOf(_afS) - Double.valueOf(_afS)));
-        Double _result_2 = (0.15 * Double.valueOf(concreteGradeFc) * Double.valueOf(sectionB) * h0) / _yre;
+        Double _As_ = _M_  * 1000000/ (Double.valueOf(steelGradeFstk) * (h0 - Double.valueOf(_afS)));
+        Double _result_2 = (0.15 * Double.valueOf(concreteGradeFc) * Double.valueOf(sectionB) * h0) / (_yre * 1000);
         Double _result_3 = ((0.6 * _afCV * Double.valueOf(concreteGradeFt) * Double.valueOf(sectionB) * h0) +
-                (Double.valueOf(steelGradeFyvk) * 0.25 * Math.PI * Math.pow(Double.valueOf(hoopD),2) * h0 / Double.valueOf(hoopL))) /_yre;
+                (Double.valueOf(steelGradeFyvk) * 4 * 0.25 * Math.PI * Math.pow(Double.valueOf(hoopD),2) * h0 / Double.valueOf(hoopL))) / (_yre * 1000);
 
         System.out.println("=============== 计算结果 ===================");
         System.out.println("受剪验算 ：\n_M_  = "+ _M_ + "\n_As_ = " + _As_ + "\n_result_2 =  "+_result_2 + "\n_result_3= "+_result_3 );
 
         //4.将计算的结果回填到表格中
-        Util.insertValueToCell(table.getRow(0).getCell(2),sectionB);
-        Util.insertValueToCell(table.getRow(0).getCell(5),sectionH);
-        Util.insertValueToCell(table.getRow(1).getCell(1),floorH);
-        Util.insertValueToCell(table.getRow(1).getCell(5),damperH);
-        Util.insertValueToCell(table.getRow(2).getCell(2),stressConditionV);
-        Util.insertValueToCell(table.getRow(2).getCell(5),_M_.toString());
+        Util.insertValueToCell(table.getRow(0).getCell(2),Util.getPrecisionString(sectionB,1));
+        Util.insertValueToCell(table.getRow(0).getCell(5),Util.getPrecisionString(sectionH,1));
+        Util.insertValueToCell(table.getRow(1).getCell(1),Util.getPrecisionString(floorH,0));
+        Util.insertValueToCell(table.getRow(1).getCell(5),Util.getPrecisionString(damperH,0));
+        Util.insertValueToCell(table.getRow(2).getCell(2),Util.getPrecisionString(stressConditionV,1));
+        Util.insertValueToCell(table.getRow(2).getCell(5),Util.getPrecisionString(_M_.toString(),1));
 
         Util.insertValueToCell(table.getRow(3).getCell(2),concreteGrade);
-        Util.insertValueToCell(table.getRow(4).getCell(2),concreteGradeFck);
-        Util.insertValueToCell(table.getRow(4).getCell(5),_afS);
-        Util.insertValueToCell(table.getRow(5).getCell(2),concreteGradeFc);
-        Util.insertValueToCell(table.getRow(5).getCell(5),concreteGradeFtk);
+        Util.insertValueToCell(table.getRow(4).getCell(2),Util.getPrecisionString(concreteGradeFck,1));
+        Util.insertValueToCell(table.getRow(4).getCell(5),Util.getPrecisionString(_afS,1));
+        Util.insertValueToCell(table.getRow(5).getCell(2),Util.getPrecisionString(concreteGradeFc,1));
+        Util.insertValueToCell(table.getRow(5).getCell(5),Util.getPrecisionString(concreteGradeFtk,1));
 
         Util.insertValueToCell(table.getRow(6).getCell(2),steelGrade);
-        Util.insertValueToCell(table.getRow(7).getCell(2),steelGradeFstk);
-        Util.insertValueToCell(table.getRow(7).getCell(5),steelGradeFyvk);
+        Util.insertValueToCell(table.getRow(7).getCell(2),Util.getPrecisionString(steelGradeFstk,0));
+        Util.insertValueToCell(table.getRow(7).getCell(5),Util.getPrecisionString(steelGradeFyvk,0));
 
-        Util.insertValueToCell(table.getRow(8).getCell(2),hoopD);
-        Util.insertValueToCell(table.getRow(8).getCell(5),hoopL);
+        Util.insertValueToCell(table.getRow(8).getCell(2),Util.getPrecisionString(hoopD,0));
+        Util.insertValueToCell(table.getRow(8).getCell(5),Util.getPrecisionString(hoopL,0));
 
-        Util.insertValueToCell(table.getRow(9).getCell(2),hoopVerticlD);
-        Util.insertValueToCell(table.getRow(9).getCell(5),hoopVerticlL);
+        Util.insertValueToCell(table.getRow(9).getCell(2),Util.getPrecisionString(hoopVerticlD,0));
+        Util.insertValueToCell(table.getRow(9).getCell(5),Util.getPrecisionString(hoopVerticlL,0));
 
-        Util.insertValueToCell(table.getRow(10).getCell(2),_As_.toString());
-        Util.insertValueToCell(table.getRow(11).getCell(2),_result_2.toString());
-        Util.insertValueToCell(table.getRow(12).getCell(2),_result_3.toString());
+        Util.insertValueToCell(table.getRow(10).getCell(2),Util.getPrecisionString(_As_,0));
+        Util.insertValueToCell(table.getRow(11).getCell(2),Util.getPrecisionString(_result_2,0));
+        Util.insertValueToCell(table.getRow(12).getCell(2),Util.getPrecisionString(_result_3,0));
     }
 }
